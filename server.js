56619a62a32580b61f51ddbdad2d776d16e6d5f7
Damian Thompasionas <dthompasionas@gmail.com>
2021-08-24 10:18:51 -0400

    pushing to heroku

--- server.js
+++ server.js
@@ -15,10 +15,15 @@ app.use(express.static("public"));
 
 
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+);
 
 //require(apiRoute)(app);
 require("./routes/htmlroutes")(app);
